60430405ad58e6dd4bb57bd855bd08d1047dfb9e
TR0J3N <95092220+TR0J3N@users.noreply.github.com>
2022-02-22 15:35:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -16,14 +16,14 @@ class Config(object):
     FORWARD_TO_CHAT_ID = list(set(int(x) for x in os.environ.get("FORWARD_TO_CHAT_ID", "-100").split()))
     # Filters for Forwards
     DEFAULT_FILTERS = "video document photo audio text gif forwarded poll sticker"
-    FORWARD_FILTERS = list(set(x for x in os.environ.get("FORWARD_FILTERS", DEFAULT_FILTERS).split()))
-    BLOCKED_EXTENSIONS = list(set(x for x in os.environ.get("BLOCKED_EXTENSIONS", "").split()))
+    FORWARD_FILTERS = DEFAULT_FILTERS
+    BLOCKED_EXTENSIONS = ""
     MINIMUM_FILE_SIZE = os.environ.get("MINIMUM_FILE_SIZE", None)
-    BLOCK_FILES_WITHOUT_EXTENSIONS = bool(os.environ.get("BLOCK_FILES_WITHOUT_EXTENSIONS", False))
+    BLOCK_FILES_WITHOUT_EXTENSIONS = False
     # Forward as Copy. Value Should be True or False
-    FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+    FORWARD_AS_COPY = True
     # Sleep Time while Kang
-    SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 10))
+    SLEEP_TIME = 10
     # Heroku Management
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
